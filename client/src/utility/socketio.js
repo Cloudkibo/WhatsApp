79d1631a79e297fac23eb6e4ac7af12aee413c73
assadbintahir <asadullah.09471@khi.iba.edu.pk>
2018-08-18 15:23:42 +0500

    socket io for client server

---
+++ client/src/utility/socketio.js
@@ -0,0 +1,32 @@
+import io from 'socket.io-client'
+
+const socket = io('')
+let store
+
+var joined = false
+var myId = ''
+
+export function initiateSocket (storeObj) {
+  store = storeObj
+  socket.connect()
+}
+
+socket.on('connect', () => {
+  console.log('Setting Socket Status to true')
+})
+
+socket.on('disconnect', () => {
+  console.log('Disconnect called')
+})
+
+socket.on('message', (data) => {
+  // Emitted event
+})
+
+export function log (tag, data) {
+  console.log(`${tag}: ${data}`)
+  socket.emit('logClient', {
+    tag,
+    data
+  })
+}
--- server/config/setup.js
+++ server/config/setup.js
@@ -6,7 +6,10 @@ const TAG = 'config/setup.js'
 module.exports = function (app, config) {
   const server = http.createServer(app)
   // TODO: we will enable this while doing socket io
-  // const socket = require('socket.io').listen(server)
+  const socket = require('socket.io').listen(server)
+
+  require('./socketio').setup(socket)
+
   server.listen(config.port, () => {
     logger.serverLog(TAG, `Whatsapp server STARTED on ${
       config.port} in ${config.env} mode`)
