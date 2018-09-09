e050338c21c1c2bf82e12002d51b2fd41f59d918
dayemsiddiqui <dayem.siddiqui@khi.iba.edu.pk>
2018-09-10 00:17:47 +0500

    Added the Group Participants

--- server/api/v1/webhooks/registerWebhooks.js
+++ server/api/v1/webhooks/registerWebhooks.js
@@ -1,10 +1,11 @@
 const groupWebhook = require('./../groups/webhooks')
+const messageWebhook = require('./../messages/webhooks')
 const init = require('./initWebhooks')
 const validationSchema = require('./schema')
 
 function initRegistry () {
   init.registerCallback(validationSchema.testSchema, (payload) => { console.log('Sample Webhook Called') })
-  init.registerCallback(validationSchema.textMessageSchema, (payload) => { console.log('Text Message Received') })
+  init.registerCallback(validationSchema.textMessageSchema, messageWebhook.handleTextMessage)
   init.registerCallback(validationSchema.groupNotificationsSchema, groupWebhook.handleGroupNotifications)
 }
 
